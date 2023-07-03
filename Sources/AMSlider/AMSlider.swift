125356f495ae8e7de1edc5af6934e53817fccbca
Bogdan Petrovskiy <82778510+kefaxoo@users.noreply.github.com>
2023-07-03 19:24:53 +0300

    Update name for class

--- Sources/AMSlider/AMSlider.swift
+++ Sources/AMSlider/AMSlider.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class AMSlider: UIView {
+class CustomAMSlider: UIView {
     private var cornerRadius        : CGFloat = 0
     private var minValue            : Double = 0
     private var maxValue            : Double = 1
